91c899e0e7a499245928859f219ae11a1c23069f
Andres Ordonez <oswaldordonez@unicauca.edu.co>
2022-09-26 12:37:58 -0500

    Update1

--- README.md
+++
@@ -1,5 +0,0 @@
-# Responsive Portfolio Bellas Artes
-
-### Responsive Portfolio Website Alexa
-Responsive Portfolio Website Using Html, Css and JavaScript. It contains a Header, Home, About, Services, Portfolio, Project in mind, Team, Contact and Footer.
-
--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $to = "sistemas.campus@bellasartes.edu.co";
 $subject = "Mail From website";
 
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@campuslaboratorios.com" . "\r\n" .
+$headers = "From: noreply@laboratoriosartetecnologia.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
